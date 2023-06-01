92894b6500e1158630c964e06c36c31d2e74988d
Ahnjaewon <vutus5@naver.com>
2023-06-01 14:25:23 +0900

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import Controller.Controller;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Controller controller = new Controller(sc);
+
+        while (true) {
+            controller.selectMode();
+        }
+
 
     }
 
---
+++ Service/Service.java
@@ -0,0 +1,21 @@
+package Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+
+    private final List<Integer> list = new ArrayList<>();
+
+    public void insert(int number){
+        list.add(number);
+    }
+    private static Service instance;
+
+    public static Service getInstance(){
+        if(instance == null) {
+            instance = new Service();
+        }
+        return instance;
+    }
+}
